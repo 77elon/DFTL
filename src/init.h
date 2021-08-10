f3b1c73635b469295d77206a4d42a2cf549fc403
77elon <88618717+77elon@users.noreply.github.com>
2021-08-10 21:47:38 +0900

    08/09 Restore

--- src/init.h
+++ src/init.h
@@ -2,9 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#pragma once
-
-#define CHANNEL_NUM 5
+#define CHANNEL_NUM 4
 #define WAY_NUM 5
 #define DIE_NUM 4
 #define PLANE_NUM 4
@@ -116,8 +114,6 @@ struct ac_time_characteristics {
 	int tWHR;      //WE high to RE low
 	int tRST;      //device resetting time
 };
-
-
 
 struct ssd_info* init_ssd(struct ssd_info* ssd);
 struct channel_info* init_channel(struct channel_info* channel);
--- src/ssd.c
+++ src/ssd.c
@@ -19,10 +19,11 @@ void SSD_write (struct ssd_info * ssd, unsigned int I_ch, unsigned int I_chip, u
 
 void SSD_erase (struct ssd_info * ssd, unsigned int I_ch, unsigned int I_chip, unsigned int I_die, unsigned int I_plane, unsigned int I_block)
 {
-    unsigned int b_erase_count = ssd -> channel [I_ch].way[I_chip]. die [I_die].plane[I_plane].block [I_block].erase_count;
-    unsigned int b_lbn = ssd -> channel [I_ch].way[I_chip]. die [I_die].plane[I_plane].block [I_block].lbn;
-    unsigned int b_pbn = ssd -> channel [I_ch].way[I_chip]. die [I_die].plane[I_plane].block [I_block] . pbn;
-    int b_OOB = ssd -> channel [I_ch].way[I_chip]. die [I_die].plane[I_plane].block [I_block] . OOB;
+    /*if Block Object isDeleted*/
+    //unsigned int b_erase_count = ssd -> channel [I_ch].way[I_chip]. die [I_die].plane[I_plane].block [I_block].erase_count;
+    //unsigned int b_lbn = ssd -> channel [I_ch].way[I_chip]. die [I_die].plane[I_plane].block [I_block].lbn;
+    //unsigned int b_pbn = ssd -> channel [I_ch].way[I_chip]. die [I_die].plane[I_plane].block [I_block] . pbn;
+    //int b_OOB = ssd -> channel [I_ch].way[I_chip]. die [I_die].plane[I_plane].block [I_block] . OOB;
 
     free(ssd -> channel [I_ch].way[I_chip]. die [I_die].plane[I_plane].block [I_block] . page);
     for (int i = 0; i < PAGE_NUM; i++)
@@ -32,6 +33,7 @@ void SSD_erase (struct ssd_info * ssd, unsigned int I_ch, unsigned int I_chip, u
         init_page(erase_page);
     }
 
+    /*backed data restore*/
     //ssd -> channel [I_ch].way[I_chip]. die [I_die].plane[I_plane].block [I_block].erase_count = b_erase_count;
     //ssd -> channel [I_ch].way[I_chip]. die [I_die].plane[I_plane].block [I_block].lbn = b_lbn;
     //ssd -> channel [I_ch].way[I_chip]. die [I_die].plane[I_plane].block [I_block] . pbn = b_pbn;
